be28cb77eaa6837b1f98c7d597496b3a984d156d
Navedsk786 <suhebkhanji@gmail.com>
2025-01-25 10:31:03 +0530

    Update # 26.js

--- Priyansh/commands/# 26.js
+++ Priyansh/commands/# 26.js
@@ -14,10 +14,10 @@ module.exports.handleEvent = async ({ api, event, Users, Currencies, args, utils
   var { threadID, messageID } = event;
   let react = event.body.toLowerCase();
   if(react.includes("26") ||
-     react.includes("happy") || react.includes("/") || react.includes("#") ||
+     react.includes("happy") || react.includes("Happy Republic day") || react.includes("#") ||
 react.includes("HAPPY") ||
 react.includes("76") ||     
-react.includes("76")) {
+react.includes("🇮🇳")) {
     var msg = {
         body: `${name}𝐇𝐀𝐏𝐏𝐘 𝐑𝐄𝐏𝐔𝐁𝐋𝐈𝐂 𝐃𝐀𝐘 💐 ཫ༄𒁍≛⃝𝐌𝐑.𝐒𝐔𝐇𝐄𝐁✿✿`,attachment: fs.createReadStream(__dirname + `/noprefix/26.mp4`)
       }
