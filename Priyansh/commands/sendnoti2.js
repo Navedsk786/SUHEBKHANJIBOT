df6189b6e743c87c9611ef49498e07b00a5df215
Navedsk786 <suhebkhanji@gmail.com>
2025-03-01 10:34:54 +0530

    Update sendnoti2.js

--- Priyansh/commands/sendnoti2.js
+++ Priyansh/commands/sendnoti2.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "sendnoti2",
 	version: "1.0.2",
 	hasPermssion: 2,
-	credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+	credits: "MR CHAND",
 	description: "Send messages to groups (reply to photos/videos to be attached)!\nBetter version of sendnotiUwU",
 	commandCategory: "system",
 	usages: "[Text]",
@@ -26,13 +26,12 @@ const request = global.nodemodule["request"];
 const fs = require('fs')
 const axios = require('axios')
 
-
-
        
         var path = __dirname + `/cache/snoti.png`;
         var path = __dirname + `/cache/snoti.mp3`;
         var path = __dirname + `/cache/snoti.jpeg`;
         var path = __dirname + `/cache/snoti.jpg`;
+        var path = __dirname + `/cache/snoti.mp4`;
 
 
 var abc = event.messageReply.attachments[0].url;
@@ -47,7 +46,7 @@ var abc = event.messageReply.attachments[0].url;
 	for (const idThread of allThread) {
 		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
 		else {
-			api.sendMessage({body:" »✦\ud835\uddd4\ud835\udde1\ud835\udde1\ud835\udde2\ud835\udde8\ud835\uddd6\ud835\uddd8\ud835\udde0\ud835\uddd8\ud835\udde1\ud835\udde7\x20\ud835\uddd9\ud835\udde5\ud835\udde2\ud835\udde0\x20\ud835\udde2\ud835\uddea\ud835\udde1\ud835\uddd8\ud835\udde5\x20\ud835\udc0f\ud835\udc11\ud835\udc08\ud835\udc18\ud835\udc00\ud835\udc0d\ud835\udc12\ud835\udc07✦«\n\n" + args.join(` `),attachment: fs.createReadStream(path) }, idThread, (error, info) => {
+			api.sendMessage({body:" »Announcement from the Admin!«\n\n" + args.join(` `),attachment: fs.createReadStream(path) }, idThread, (error, info) => {
 				if (error) cantSend.push(idThread);
 			});
 			count++;
@@ -64,7 +63,7 @@ else {
 	for (const idThread of allThread) {
 		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
 		else {
-			api.sendMessage("»\x41\x6e\x6e\x6f\x75\x6e\x63\x65\x6d\x65\x6e\x74\x20\x66\x72\x6f\x6d\x20\x74\x68\x65\x20\x41\x64\x6d\x69\x6e\x20\x50\x72\x69\x79\x61\x6e\x73\x68\x21«\n\n" + args.join(` `), idThread, (error, info) => {
+			api.sendMessage("»Announcement from the Admin!«\n\n" + args.join(` `), idThread, (error, info) => {
 				if (error) cantSend.push(idThread);
 			});
 			count++;
