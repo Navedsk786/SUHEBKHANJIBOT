d04d293ce5ee2466147bf74d820f0bf5b458a482
Navedsk786 <suhebkhanji@gmail.com>
2025-06-07 06:20:00 +0530

    Update Gana.js

--- Priyansh/commands/Gana.js
+++ Priyansh/commands/Gana.js
@@ -13,7 +13,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("gana") ||
-     react.includes("") || react.includes("SONG") || react.includes("") ||
+     react.includes("GANA") || react.includes("SONG") || react.includes("GANA") ||
 react.includes("Gana") ||
 react.includes("GANA")) {
 		var msg = {
